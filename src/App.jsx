55dad8d0668b2dc24181bd58b832d19031c0ad74
SatishSingh0313 <satishsangam.205@gmail.com>
2024-02-26 00:59:13 +0530

    frontend files

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Footer from './components/Footer';
 import Nav from './components/Nav'
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import SignUp from './components/SignUp';
+import PrivatComponent from './components/PrivateComponent';
 function App() {
   return (
     <div className="App">
@@ -10,11 +11,15 @@ function App() {
      <Nav/>
 
      <Routes>
-      <Route path="/" element={<h1>Product Listing Component</h1>}></Route>
+      <Route element={<PrivatComponent/>}/>
+
+      <Route path="/" element={<h1>Product Listing Component</h1>}>
       <Route path="/add" element={<h1> Add Product Component</h1>}></Route>
       <Route path="/update" element={<h1> Update Product Component</h1>}></Route>
       <Route path="/logout" element={<h1> Logout Component</h1>}></Route>
       <Route path="/profile" element={<h1> Profile Product Component</h1>}></Route>
+      </Route>
+      
       <Route path="/signup" element={<SignUp/>}></Route>
      </Routes>
     </BrowserRouter>
