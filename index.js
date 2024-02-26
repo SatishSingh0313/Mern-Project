2ec21251a7522ee84fd7772d37bdf473183fd0bb
SatishSingh0313 <satishsangam.205@gmail.com>
2024-02-27 01:13:17 +0530

    index.js

--- index.js
+++ index.js
@@ -54,4 +54,22 @@ app.delete("/product/:id",async(req,resp)=>{
     resp.send(result);
 })
 
+app.get("/product/:id", async (req,resp)=>{
+    let result=await Product.findOne({_id:req.params.id});
+    if(result){
+        resp.send(result)
+    }else{
+        resp.send({result:"No Product Found"})
+    }
+   
+})
+
+app.put("/product/:id", async(req,resp)=>{
+     let result=await Product.updateOne(
+        {_id:req.params.id},
+        {$set:req.body}
+     ) 
+     resp.send(result)
+});
+
 app.listen(5000);
